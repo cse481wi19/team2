398d780642ce90c2dff66d0f475db872ae2f2b52
Kiet <kietmsm@gmail.com>
2019-02-12 16:28:07 -0800

    w5 - lab25 done

--- applications/scripts/check_cart_pose.py
+++ applications/scripts/check_cart_pose.py
@@ -20,34 +20,34 @@ def print_usage():
 def main():
     rospy.init_node('check_cart_pose')
     wait_for_time()
-    # argv = rospy.myargv()
-    # if len(argv) < 5:
-    #     print_usage()
-    #     return
-    # command, x, y, z = argv[1], float(argv[2]), float(argv[3]), float(argv[4])
+    argv = rospy.myargv()
+    if len(argv) < 5:
+        print_usage()
+        return
+    command, x, y, z = argv[1], float(argv[2]), float(argv[3]), float(argv[4])
    
-    # arm = robot_api.Arm()
-    # ps = PoseStamped()
-    # ps.header.frame_id = 'base_link'
-    # ps.pose.position.x = x
-    # ps.pose.position.y = y
-    # ps.pose.position.z = z
-    # ps.pose.orientation.w = 1
+    arm = robot_api.Arm()
+    ps = PoseStamped()
+    ps.header.frame_id = 'base_link'
+    ps.pose.position.x = x
+    ps.pose.position.y = y
+    ps.pose.position.z = z
+    ps.pose.orientation.w = 1
            
-    # if command == 'plan':
-    #     error = arm.check_pose(ps, allowed_planning_time=1.0)
-    #     if error is None:
-    #         rospy.loginfo('Found plan!')
-    #     else:       
-    #         rospy.loginfo('No plan found.')
-    #     arm.cancel_all_goals()
-    # elif command == 'ik':
-    #     if arm.compute_ik(ps):
-    #         rospy.loginfo('Found IK!')
-    #     else:
-    #         rospy.loginfo('No IK found.')
-    # else:           
-    #     print_usage()
+    if command == 'plan':
+        error = arm.check_pose(ps, allowed_planning_time=1.0)
+        if error is None:
+            rospy.loginfo('Found plan!')
+        else:       
+            rospy.loginfo('No plan found.')
+        arm.cancel_all_goals()
+    elif command == 'ik':
+        if arm.compute_ik(ps):
+            rospy.loginfo('Found IK!')
+        else:
+            rospy.loginfo('No IK found.')
+    else:           
+        print_usage()
                     
                     
 if __name__ == '__main__':
