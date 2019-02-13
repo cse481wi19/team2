30b5cae6796ee5642470bb7535b7af0ec4574052
Kiet <kietmsm@gmail.com>
2019-02-12 17:42:27 -0800

    w5 - lab26 interactions implemented

--- applications/scripts/gripper_teleop.py
+++ applications/scripts/gripper_teleop.py
@@ -4,9 +4,10 @@ import robot_api
 import rospy
 import copy
 
-from geometry_msgs.msg import Pose
+from geometry_msgs.msg import Pose, PoseStamped
+from std_msgs.msg import ColorRGBA
 from interactive_markers.interactive_marker_server import InteractiveMarkerServer
-from visualization_msgs.msg import InteractiveMarker, InteractiveMarkerControl, InteractiveMarkerFeedback
+from visualization_msgs.msg import InteractiveMarker, InteractiveMarkerControl, InteractiveMarkerFeedback, MenuEntry
 from visualization_msgs.msg import Marker
 
 def wait_for_time():
@@ -19,6 +20,10 @@ def wait_for_time():
 GRIPPER_MESH = 'package://fetch_description/meshes/gripper_link.dae'
 L_FINGER_MESH = 'package://fetch_description/meshes/l_gripper_finger_link.STL'
 R_FINGER_MESH = 'package://fetch_description/meshes/r_gripper_finger_link.STL'
+
+ID_GOTO = 1
+ID_OPEN = 2
+ID_CLOSE = 3
 
 class GripperTeleop(object):
     def __init__(self, arm, gripper, im_server):
@@ -66,11 +71,23 @@ class GripperTeleop(object):
         controls.append(copy.deepcopy(control))
         return controls
 
-    def start(self):
-        pose = Pose()
-        pose.position.x = 0
-        pose.position.y = 0
-        pose.position.z = 0
+    def update_marker(self, is_color_good, pose=None):
+        if pose is None:
+            pose = Pose()
+        
+        color = ColorRGBA()
+        if is_color_good:
+            color.r = 0.5
+            color.g = 1
+            color.b = 0.5
+            color.a = 1
+        else:
+            color.r = 1
+            color.g = 0.25
+            color.b = 0.25
+            color.a = 1
+        
+        x_offset = 0.166
 
         gripper_im = InteractiveMarker()
         gripper_im.header.frame_id = "base_link"
@@ -85,11 +102,8 @@ class GripperTeleop(object):
         gripper_marker.scale.x = 1
         gripper_marker.scale.y = 1
         gripper_marker.scale.z = 1
-        gripper_marker.color.r = 0.5
-        gripper_marker.color.g = 0.5
-        gripper_marker.color.b = 0.5
-        gripper_marker.color.a = 1
-        gripper_marker.pose.position.x = 0.0166
+        gripper_marker.color = color
+        gripper_marker.pose.position.x = x_offset
 
         r_gripper_marker = Marker()
         r_gripper_marker.text = "r_gripper_tip"
@@ -98,12 +112,9 @@ class GripperTeleop(object):
         r_gripper_marker.scale.x = 1
         r_gripper_marker.scale.y = 1
         r_gripper_marker.scale.z = 1
-        r_gripper_marker.color.r = 0.5
-        r_gripper_marker.color.g = 0.5
-        r_gripper_marker.color.b = 0.5
-        r_gripper_marker.color.a = 1
+        r_gripper_marker.color = color
         r_gripper_marker.pose.position.y = 0.05
-        r_gripper_marker.pose.position.x = 0.0166
+        r_gripper_marker.pose.position.x = x_offset
 
         l_gripper_marker = Marker()
         l_gripper_marker.text = "l_gripper_tip"
@@ -112,12 +123,9 @@ class GripperTeleop(object):
         l_gripper_marker.scale.x = 1
         l_gripper_marker.scale.y = 1
         l_gripper_marker.scale.z = 1
-        l_gripper_marker.color.r = 0.5
-        l_gripper_marker.color.g = 0.5
-        l_gripper_marker.color.b = 0.5
-        l_gripper_marker.color.a = 1
+        l_gripper_marker.color = color
         l_gripper_marker.pose.position.y = -0.05
-        l_gripper_marker.pose.position.x = 0.0166
+        l_gripper_marker.pose.position.x = x_offset
 
         gripper_control = InteractiveMarkerControl()
         gripper_control.name = "gripper_link_control"
@@ -131,10 +139,65 @@ class GripperTeleop(object):
         gripper_im.controls.append(gripper_control)
         gripper_im.controls.extend(self.make_6dof_controls())
 
+        goto_me = MenuEntry()
+        goto_me.id = ID_GOTO
+        goto_me.title = "Gripper Goto Here"
+        goto_me.command_type = MenuEntry.FEEDBACK
+
+        open_me = MenuEntry()
+        open_me.id = ID_OPEN
+        open_me.title = "Gripper Open"
+        open_me.command_type = MenuEntry.FEEDBACK
+
+        close_me = MenuEntry()
+        close_me.id = ID_CLOSE
+        close_me.title = "Gripper Close"
+        close_me.command_type = MenuEntry.FEEDBACK
+
+        gripper_im.menu_entries.append(goto_me)
+        gripper_im.menu_entries.append(open_me)
+        gripper_im.menu_entries.append(close_me)
+
         self._im_server.insert(gripper_im, feedback_cb=self.handle_feedback)
         self._im_server.applyChanges()
 
+    def start(self):
+        ps = PoseStamped()
+        ps.header.frame_id = "base_link"
+        if self._arm.compute_ik(ps):
+            # Found IK
+            self.update_marker(True)
+        else:
+            # Didn't find IK
+            self.update_marker(False)
+
     def handle_feedback(self, feedback):
+        if feedback.event_type == InteractiveMarkerFeedback.MENU_SELECT:
+            print(feedback)
+            if feedback.menu_entry_id == ID_GOTO:
+                print("GOTO PRESSED")
+                ps = PoseStamped()
+                ps.pose = feedback.pose
+                ps.header.frame_id = "base_link"
+                self._arm.move_to_pose(ps)
+            elif feedback.menu_entry_id == ID_OPEN:
+                print("OPEN PRESSED")
+                self._gripper.open()
+            elif feedback.menu_entry_id == ID_CLOSE:
+                print("CLOSE PRESSED")
+                self._gripper.close()
+        elif feedback.event_type == InteractiveMarkerFeedback.POSE_UPDATE:
+            ps = PoseStamped()
+            ps.pose = feedback.pose
+            ps.header.frame_id = "base_link"
+            if self._arm.compute_ik(ps):
+                # Found IK
+                self.update_marker(True, pose=feedback.pose)
+            else:
+                # Didn't find IK
+                self.update_marker(False, pose=feedback.pose)
+        else:
+            print(feedback)
         pass
 
 
