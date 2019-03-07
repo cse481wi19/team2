ae56b11d10a512cd259568e2c60af691082e6471
RoboEats <roboeats1@gmail.com>
2019-03-07 14:43:55 -0800

    Added planning scene for open door microwave

--- roboeats/nodes/part1_open_obstacles.py
+++ roboeats/nodes/part1_open_obstacles.py
@@ -32,10 +32,11 @@ def main():
     microwave_width = 0.48
     microwave_depth = 0.33
 
-    planning_scene.addBox('microwave', microwave_depth, microwave_width, microwave_height, table_x, 0.1, table_height + microwave_height/2)
-#     planning_scene.addBox('microwave_side_r', 0.54, 0.52, 0.37, 0, 0, 0.37/2)
-#     planning_scene.addBox('microwave_side_l', 0.54, 0.52, 0.37, 0, 0, 0.37/2)
-#     planning_scene.addBox('microwave_back', 0.54, 0.52, 0.37, 0, 0, 0.37/2)
+    planning_scene.addBox('microwave_top', microwave_depth, microwave_width, 0.04, table_x, 0.1, table_height + microwave_height + 0.02)
+    planning_scene.addBox('microwave_side_r', microwave_depth, 0.135, 0.2, table_x, -0.1, table_height + microwave_height/2)
+    # planning_scene.addBox('microwave_side_l', microwave_depth, microwave_width, microwave_height, table_x, 0.1, table_height + microwave_height/2)
+    # planning_scene.addBox('microwave_back', microwave_depth, microwave_width, microwave_height, table_x, 0.1, table_height + microwave_height/2)
+    # planning_scene.addBox('microwave_door', microwave_depth, microwave_width, microwave_height, table_x, 0.1, table_height + microwave_height/2)
 
     rospy.sleep(2)
     rospy.spin()
