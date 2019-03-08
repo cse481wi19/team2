8c2286e1f6e17aabaa4b2c24bd5a502aadee1ee5
Team 2 <roboeats1@gmail.com>
2019-03-07 16:53:48 -0800

    Obstacles for close and open microwave door

--- roboeats/nodes/part1_obstacles.py
+++ roboeats/nodes/part1_obstacles.py
@@ -31,8 +31,10 @@ def main():
     microwave_height = 0.28
     microwave_width = 0.48
     microwave_depth = 0.33
+    microwave_x = 0.97
+    microwave_y = 0.18
 
-    planning_scene.addBox('microwave', microwave_depth, microwave_width, microwave_height, table_x, 0.1, table_height + microwave_height/2)
+    planning_scene.addBox('microwave', microwave_depth, microwave_width, microwave_height, microwave_x, microwave_y, table_height + microwave_height/2)
 #     planning_scene.addBox('microwave_side_r', 0.54, 0.52, 0.37, 0, 0, 0.37/2)
 #     planning_scene.addBox('microwave_side_l', 0.54, 0.52, 0.37, 0, 0, 0.37/2)
 #     planning_scene.addBox('microwave_back', 0.54, 0.52, 0.37, 0, 0, 0.37/2)
