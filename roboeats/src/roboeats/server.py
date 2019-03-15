2ebe8bac3c026d68e8181e214b8ba33ca6865644
RoboEats <roboeats1@gmail.com>
2019-03-15 16:39:28 -0700

    fix indent 3

--- roboeats/src/roboeats/server.py
+++ roboeats/src/roboeats/server.py
@@ -243,15 +243,13 @@ class RoboEatsServer(object):
         microwave_door_x = microwave_x - 0.33
         microwave_door_y = microwave_l_y + 0.027
 
-
         self.planning_scene.addBox('microwave_top', microwave_depth, microwave_width, microwave_top_height, microwave_x, microwave_y, table_height + microwave_z + microwave_bottom_height + microwave_side_height + (microwave_top_height/2))
         self.planning_scene.addBox('microwave_bottom', microwave_depth, microwave_width, microwave_bottom_height, microwave_x, microwave_y, table_height + microwave_z + (microwave_bottom_height/2))
         self.planning_scene.addBox('microwave_side_r', microwave_depth, microwave_r_width, microwave_side_height, microwave_x, microwave_r_y, table_height + microwave_z + microwave_bottom_height + (microwave_side_height/2))
         self.planning_scene.addBox('microwave_side_l', microwave_depth, microwave_l_width, microwave_side_height, microwave_x, microwave_l_y, table_height + microwave_z + microwave_bottom_height +  microwave_side_height/2)
         self.planning_scene.addBox('microwave_back', microwave_back_depth, microwave_width, microwave_height, microwave_back_x, microwave_y, table_height + microwave_z + microwave_height/2)
         self.planning_scene.addBox('microwave_door', 0.39, microwave_door_width, microwave_height + 0.01, microwave_door_x, microwave_door_y, table_height + microwave_z + microwave_height/2 + 0.005)
-
-	    self.attach_lunchbox()
+        self.attach_lunchbox()
 
     def attach_lunchbox(self):
         frame_attached_to = 'gripper_link'
